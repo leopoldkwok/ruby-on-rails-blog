a257b917d3e599aa9362a45cc3106848108cc243
leopoldkwok <leopoldkwok@gmail.com>
2014-12-02 18:43:01 +0000

    created the initial elements for a ruby on rails blog

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+# We're going to add in some functionality to our new Rails app which enforces a rule that 
+# every post must have a title
+  
+  # attr_accessible :body, :title - we donot need to use attr_accessible anymores
+
+  validates_presence_of :body, :title
+
+end
